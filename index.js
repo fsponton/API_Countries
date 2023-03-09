c143a6a858f0562d0c9aa59174f3062c8bf7a263
Francisco Sponton <fsponton@Franciscos-Air.fibertel.com.ar>
2023-03-09 17:05:38 -0300

    add method http

--- index.js
+++ index.js
@@ -19,10 +19,14 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const http = require('http')
+
+const serverAPP = http.createServer(server)
+
 
 // Syncing all the models at once.
 conn.sync(/*{ force: true }*/).then(() => {
-  server.listen(3002, () => {
+  serverAPP.listen(3002, () => {
     console.log('%s listening at 3002'); // eslint-disable-line no-console
   });
 });
