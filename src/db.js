e61251a43a004531abe53ab2275b646870d9218a
Francisco Sponton <32375657+fsponton@users.noreply.github.com>
2023-08-30 19:11:52 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -40,14 +40,12 @@ modelDefiners.forEach(model => model(sequelize));
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
-
 // En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
+
+// Para relacionarlos hago destructuring
 const { Country, Activity } = sequelize.models;
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-
+// Relacion entre tablas.
 Country.belongsToMany(Activity, { through: "activityCountry" })
 Activity.belongsToMany(Country, { through: "activityCountry" })
 
