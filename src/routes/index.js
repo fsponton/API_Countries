204a3b610d3509a9170cc3397a6333f07ca5698a
Francisco Sponton <32375657+fsponton@users.noreply.github.com>
2023-08-30 19:09:13 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,8 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const activities = require('./activitiesRouter');
 const countries = require('./countriesRouter')
 
 const router = Router();
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 router.use('/countries', countries);
 router.use('/activities', activities);
